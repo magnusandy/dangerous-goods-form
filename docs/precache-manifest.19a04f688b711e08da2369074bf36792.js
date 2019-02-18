0ac6cf04ea7d284529c2f7e97294043eb438ccf7
Andrew Magnus <magnusandy@gmail.com>
2019-02-18 14:43:10 -0600

    the docs thing kinda sucks but whatever

---
+++ docs/precache-manifest.19a04f688b711e08da2369074bf36792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "d2df1ea0b343cbc3372f",
+    "url": "./static/js/main.d2df1ea0.chunk.js"
+  },
+  {
+    "revision": "a2aa94c918406e66ee8d",
+    "url": "./static/js/2.a2aa94c9.chunk.js"
+  },
+  {
+    "revision": "d2df1ea0b343cbc3372f",
+    "url": "./static/css/main.46cdbb43.chunk.css"
+  },
+  {
+    "revision": "5eefee0ed5021da7e816c25911c13931",
+    "url": "./index.html"
+  }
+];
