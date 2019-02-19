f26fa19a4e47f838a11a2f27bed315860e608268
Andrew Magnus <magnusandy@gmail.com>
2019-02-19 00:10:54 -0600

    electron test branch

---
+++ electronApp/docs/precache-manifest.76c96d8351dce9f3e811c64a081144a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "95653623597e3cc44186",
+    "url": "./static/js/main.95653623.chunk.js"
+  },
+  {
+    "revision": "a2aa94c918406e66ee8d",
+    "url": "./static/js/2.a2aa94c9.chunk.js"
+  },
+  {
+    "revision": "95653623597e3cc44186",
+    "url": "./static/css/main.46cdbb43.chunk.css"
+  },
+  {
+    "revision": "c02d11d88429a5479221d91031bf6821",
+    "url": "./index.html"
+  }
+];
